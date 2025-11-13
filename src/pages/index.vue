badb2b173effc54a6b645a86b4ece1ef2acd2a8f
缪凯杰 <miaokaijie@yiqice-ai.cn>
2025-11-13 10:01:09 +0800

    feat: 添加 uni-use-router 依赖并更新首页跳转功能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+import { Base64 } from 'js-base64'
+import { useRouter } from 'uni-use-router'
+
 definePage({
   layout: false,
   style: { navigationStyle: 'custom' },
 })
+
+// 仿vue-router
+const router = useRouter()
+
+function jump() {
+  const url = 'https://uni-helper.js.org/vitesse-uni-app/getting-started/introduction'
+  const encodedUrl = Base64.encode(url)
+  router.push({
+    url: '/pages/WebView',
+    query: {
+      url: encodedUrl,
+    },
+  })
+}
 </script>
 
 <template>
@@ -23,8 +40,9 @@ definePage({
       transition-all
       duration-300
       ease-in-out
-      hover="scale-110 text-#1a73e8"
-      class="[text-shadow:2px_2px_4px_rgba(0,0,0,0.1)] rotate-[-15deg] hover:rotate-[-12deg]"
+      hover="scale-110"
+      class="[text-shadow:2px_2px_4px_rgba(0,0,0,0.1)] rotate-[-15deg] hover:rotate-[-12deg] hover:text-[#1a73e8]"
+      @click="jump"
     >
       Welcome
     </view>
